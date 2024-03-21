7129156c6082bca4f71966c9e937ebeba1247007
jahansher333 <160265854+jahansher333@users.noreply.github.com>
2024-03-21 20:20:29 +0000

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstnumber" },
+    { message: "Enter your second number", type: "number", name: "secondnumber" },
+    { message: "Selecter one of the operater to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multipliction", "Division"]
+    }
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multipliction") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("error unvalid number");
+}
